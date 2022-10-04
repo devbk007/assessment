5427ab9cb9df89dafbb2ae6812e67a68533d46d8
Baizel Kurian Varghese <baizelkv@gmail.com>
2022-10-04 20:14:55 +0530

    Connect to database and add document to database

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+
+from mongoengine import connect
+from models import CountryDetails
+
+# Connecting to MongoDB
+connect(db="countries", host="localhost", port=27017)
+
+url="https://restcountries.com/v3.1/all"
+
+z=requests.get(url)
+
+data=z.json()
+count = 0
+for i in data:
+    try:
+        country = CountryDetails(
+            common_name = i["name"]["common"],
+            official_name = i["name"]["official"],
+            capital = i["capital"][0],
+            region = i["region"],
+            subregion = i["subregion"],
+            latlng = i["latlng"],
+            population = i["population"],
+            un_member = i["unMember"],
+            landlocked = i["landlocked"],
+            area = i["area"]
+        )
+        country.save()
+
+    except:
+        continue
+
+print("Successfully entered data in the database")
