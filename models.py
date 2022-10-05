49afa60cdc403b73c08d082e6a948314f1113a2c
Baizel Kurian Varghese <baizelkv@gmail.com>
2022-10-05 12:42:25 +0530

    Add languages field and remove landlocked field

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
 # Schema
 
-from mongoengine import Document, StringField, IntField , BooleanField, PointField, DecimalField  # PointField for latlng coordinates
+from mongoengine import Document, StringField, IntField , BooleanField, PointField, DecimalField, DictField
 
 class CountryDetails(Document):
     id = IntField()
     common_name = StringField(max_length=200)
     official_name = StringField(max_length=200)
     capital = StringField(max_length=200)
+    languages = DictField()
     region = StringField(max_length=200)
     subregion = StringField(max_length=200)
     latlng = PointField()
     population = IntField()
     un_member = BooleanField()
-    landlocked = BooleanField()
     area = DecimalField()
