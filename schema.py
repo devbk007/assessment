05a17ea153060204a120d1c342fac55434671e2b
Baizel Kurian Varghese <baizelkv@gmail.com>
2022-10-06 13:10:35 +0530

    Add countriesNearbyQuery

--- schema.py
+++ schema.py
@@ -3,6 +3,7 @@ from graphene_mongo import MongoengineObjectType
 from models import CountryDetails as CountryDetailsModel
 from mongoengine import connect
 import json
+from haversine import haversine, Unit
 
 connect(db="countries", host="localhost", port=27017)
 
@@ -27,8 +28,15 @@ class Query(graphene.ObjectType):
         language = graphene.String()
         )
 
+    countriesNearbyQuery = graphene.List(
+        CountryDetail,
+        latlng = graphene.List(graphene.Float, graphene.Float),
+        distance = graphene.Float()
+    )
+
     def resolve_countriesQuery(parent, info, first=None, skip=None):
         qs = list(CountryDetailsModel.objects.all())
+
         if skip:
                 qs = qs[skip:]
 
@@ -42,6 +50,16 @@ class Query(graphene.ObjectType):
     def resolve_countriesByLanguageQuery(parent, info, language):
         return CountryDetailsModel.objects.filter(languages__contains=language)
 
+    def resolve_countriesNearbyQuery(parent, info, latlng, distance):
+        nearby_countries = []
+
+        for e in CountryDetailsModel.objects.all():
+            calculated_distance = haversine((e.latlng[0], e.latlng[1]), (latlng[0], latlng[1]))
+            if calculated_distance <= distance:
+                nearby_countries.append(e.common_name)
+
+        return CountryDetailsModel.objects.filter(common_name__in=nearby_countries)
+        # return CountryDetailsModel.objects.all()
 
 schema = graphene.Schema(query=Query, auto_camelcase=False)
 
@@ -90,16 +108,40 @@ schema = graphene.Schema(query=Query, auto_camelcase=False)
 #     '''
 # )
 
+# result = schema.execute(
+#     '''
+#     {
+#         countriesByLanguageQuery (language:"Spanish"){
+#             common_name
+#         }
+#     }
+#     '''
+# )
+
+# # print(result)
+# items = dict(result.data.items())
+# print(json.dumps(items, indent=4))
+
 result = schema.execute(
     '''
     {
-        countriesByLanguageQuery (language:"Spanish"){
+        countriesNearbyQuery (latlng:[47.0,20.0], distance:100){
+            _id
             common_name
+            official_name
+            capital
+            languages
+            region
+            subregion
+            latlng
+            population
+            un_member
+            area
         }
     }
     '''
 )
 
-# print(result)
+# print(result.errors)
 items = dict(result.data.items())
 print(json.dumps(items, indent=4))
